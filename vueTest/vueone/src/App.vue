6c7a18bd119c8529da243ee11decfe64797f58a1
Tinet-wangtao <wangtao@ti-net.com.cn>
2017-04-21 15:41:00 +0800

    vue框架添加组件的测试的代码示例

---
+++ vueTest/vueone/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>hohoTT</h1>
+    <p>{{msg}}</p>
+    <firstcomponent></firstcomponent>
+  </div>
+</template>
+
+<script>
+
+  // 引入组件
+  import firstcomponent from './components/firstcomponent.vue'
+
+  export default {
+    name: 'app',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    components: { firstcomponent }
+  }
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ vueTest/vueone/src/components/firstcomponent.vue
@@ -0,0 +1,19 @@
+<template>
+<div id="firstcomponent">
+    <h1>This is a component's title.</h1>
+    <a> written by {{ author }} </a>
+</div>
+</template>
+
+<script type="text/javascript">
+    export default {
+        data () {
+            return {
+                author: "hohoTT"
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
